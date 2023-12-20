eb361daf7b62b70fa3f3a1d577f9c0176fe932c7
fmentzer <63960167+fmentzer@users.noreply.github.com>
2023-12-20 12:33:01 +0100

    Add GIVT

--- assets/js/script.js
+++ assets/js/script.js
@@ -52,6 +52,12 @@ class Publication {
 
 const PUBLICATIONS = [
     new Publication(
+        "GIVT: Generative Infinite-Vocabulary Transformers",
+        "Michael Tschannen, Cian Eastwood, Fabian Mentzer",
+        "preprint (2023)",
+        "https://arxiv.org/abs/2312.02116",
+        ),
+    new Publication(
         "Finite Scalar Quantization: VQ-VAE Made Simple",
         "Fabian Mentzer, David Minnen, Eirikur Agustsson, Michael Tschannen",
         "preprint (2023)",
